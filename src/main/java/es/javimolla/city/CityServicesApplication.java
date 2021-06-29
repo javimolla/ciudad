93cc08c9563a2a7529a9a60c6b3d22cb2d6fe5e3
Javi Mollá <javier.molla.moreno@gmail.com>
2021-06-29 17:41:24 +0200

    Create project skeleton

---
+++ src/main/java/es/javimolla/city/CityServicesApplication.java
@@ -0,0 +1,13 @@
+package es.javimolla.city;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/javimolla/city/CityServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.javimolla.city;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
