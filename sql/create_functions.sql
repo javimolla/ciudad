c14d7f1827061732ae6a6dbe7a1f4df9ab450adc
Javi Mollá <javier.molla.moreno@gmail.com>
2021-07-06 18:00:46 +0200

    Add sql to create the database objects

---
+++ sql/create_functions.sql
@@ -0,0 +1,62 @@
+-- This function generates the isochrone of a vertex from the OSM network
+-- TODO: It uses 1.250 meters as a fixed distance, but it could receive it by parameter
+-- TODO: It could also have a parameter of the radius applied to the alpha shape
+CREATE OR REPLACE FUNCTION get_isochrone(vertex_id integer)
+ RETURNS geometry
+ LANGUAGE plpgsql
+AS $FUNCTION$
+DECLARE
+isochrone geometry;
+BEGIN
+	DROP TABLE IF EXISTS isochrones_temp;
+	CREATE TABLE isochrones_temp AS (
+		WITH ways_lines AS (
+			SELECT vertex_id AS gid, ST_Transform(l.the_geom, 25830) AS the_geom
+			FROM pgr_drivingDistance('SELECT gid AS id, source, target, length_m AS cost FROM ways', vertex_id, 1250, false) AS foo join ways l ON l.gid = foo.edge
+		), ways_points AS (
+			SELECT gid, ST_StartPoint(the_geom) AS the_geom FROM ways_lines
+			UNION
+			SELECT gid, ST_EndPoint(the_geom) AS the_geom FROM ways_lines
+		)
+		SELECT gid, the_geom FROM ways_points
+	);
+	WITH alphashape AS (
+		SELECT pgr_alphaShape('SELECT gid AS id, ST_X(the_geom) AS x, ST_Y(the_geom) AS y FROM isochrones_temp WHERE gid = ' || vertex_id)
+	), alphapoints AS (
+		SELECT ST_SetSRID(ST_MakePoint((pgr_alphaShape).x, (pgr_alphaShape).y), 25830) FROM alphashape
+	), alphaline AS (
+		SELECT ST_Makeline(ST_SetSRID) FROM alphapoints
+	)
+	SELECT ST_Buffer(ST_MakePolygon(ST_AddPoint(ST_Makeline, ST_StartPoint(ST_Makeline))), 30) AS the_geom FROM alphaline INTO isochrone;
+	RETURN isochrone;
+ EXCEPTION
+	WHEN OTHERS THEN
+		RETURN NULL;
+END;
+$FUNCTION$
+;
+
+-- This function finds the nearest vertex of the OSM network to the geometry of a portal, then it generates the isochrone by calling the get_isochrone function and, finally, stores the isochrone and its relationship with the portal on an intermediate table
+-- TODO: It checks if the isochrone has already been generated and it does not generate it again. It could receive a parameter indicating if we want this check or not
+CREATE OR REPLACE FUNCTION calculate_isochrones(geom geometry, geom_id integer)
+ RETURNS boolean
+ LANGUAGE plpgsql
+AS $FUNCTION$
+DECLARE
+vertex_id int4;
+origin_check int4;
+BEGIN
+	SELECT id::int4 FROM ways_vertices_pgr order by ST_Distance(the_geom, ST_Transform(geom, 4326)) LIMIT 1 INTO vertex_id;
+	SELECT COUNT(*) FROM isochrones WHERE gid = vertex_id INTO origin_check;
+	IF (origin_check = 0) THEN
+        INSERT INTO isochrones (gid, isochrone) VALUES (vertex_id, get_isochrone(vertex_id));
+	END IF;
+	DELETE FROM isochrones_portals WHERE gid = geom_id;
+	INSERT INTO isochrones_portals (gid, isochrone) VALUES (geom_id, vertex_id);
+	RETURN true;
+ EXCEPTION
+	WHEN OTHERS THEN
+		RETURN false;
+END;
+$FUNCTION$
+;
---
+++ sql/create_tables.sql
@@ -0,0 +1,45 @@
+-- Example of a table that stores the city streets
+CREATE TABLE streets (
+	gid int4 NOT NULL,
+	street_id int4 NOT NULL,
+	street_name varchar(75) NOT NULL,
+	CONSTRAINT streets_pkey PRIMARY KEY (gid)
+);
+CREATE UNIQUE INDEX streets_id_idx ON streets USING btree (street_id);
+
+-- Example of a table that stores the city addresses
+CREATE TABLE portals (
+	gid int4 NOT NULL,
+	street_id int4 NOT NULL,
+	portal_number int4 NULL,
+	the_geom geometry NULL,
+	CONSTRAINT portals_pk PRIMARY KEY (gid),
+	CONSTRAINT streets_portals_fk FOREIGN KEY (street_id) REFERENCES streets(street_id) ON DELETE CASCADE
+);
+CREATE INDEX portals_geom_idx ON portals USING GIST(the_geom);
+
+-- This table stores the generated isochrones
+CREATE TABLE isochrones (
+	gid int4 NOT NULL,
+	the_geom geometry NULL,
+	CONSTRAINT isochrones_pk PRIMARY KEY (gid)
+);
+CREATE INDEX isochrones_geom_idx ON isochrones USING GIST(the_geom);
+
+-- This table stores the relationship between the portal and its isochrone
+CREATE TABLE isochrones_portals (
+	gid int4 NOT NULL,
+	isochrone int4 NOT NULL,
+	CONSTRAINT isochrones_portals_pk PRIMARY KEY (gid, isochrone),
+	CONSTRAINT isochrones_isochro_portals_fk FOREIGN KEY (isochrone) REFERENCES isochrones(gid) ON DELETE CASCADE,
+	CONSTRAINT portals_isochro_portals_fk FOREIGN KEY (gid) REFERENCES portals(gid) ON DELETE CASCADE
+);
+
+-- This table stores the number of equipments by layer
+CREATE TABLE isochrones_statistics (
+	isochrone int4 NOT NULL,
+	layer text NOT NULL,
+	equipments int4 NOT NULL,
+	CONSTRAINT isochrones_statistics_pk PRIMARY KEY (isochrone, layer),
+	CONSTRAINT isochrones_isochro_stats_fk FOREIGN KEY (isochrone) REFERENCES isochrones(gid) ON DELETE CASCADE
+);
